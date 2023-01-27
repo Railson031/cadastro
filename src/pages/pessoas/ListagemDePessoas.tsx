d367fe74a2bd8c0dbf75b94a133b7842b0330faf
Railson Carvalho <railson031@hotmail.com>
2023-01-27 14:46:07 -0300

    feat: Adicionando tabela para listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,31 +1,50 @@
-import { debounce } from "@mui/material";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow
+} from "@mui/material";
 
-import { FerramentasDaListagem } from "../../shared/components";
 import { useDebounce } from "../../shared/hooks";
 import { LayoutBaseDePagina } from "../../shared/layouts";
-import { PessoasService } from "../../shared/services";
-
+import { FerramentasDaListagem } from "../../shared/components";
+import { IListagemPessoa, PessoasService } from "../../shared/services";
 
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
   const { debounce } = useDebounce(3000, false);
 
+  const [rows, setRows] = useState<IListagemPessoa[]>([]);
+  const [totalCount, setTotalCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     debounce(() => {
-      PessoasService.getAll(1, busca).then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+
+            setTotalCount(result.totalCount);
+            setRows(result.data);
+          }
+        });
     });
   }, [busca]);
 
@@ -38,10 +57,33 @@ export const ListagemDePessoas: React.FC = () => {
             mostrarInputBusca
             textoBotaoNovo='Nova'
             textoBusca={busca}
-            aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
+            aoMudarTextoDeBusca={
+              texto => setSearchParams({ busca: texto },
+                { replace: true })
+            }
           />
         }
       >
+        <TableContainer component={Paper} variant='outlined' sx={{ m: 1, width: 'auto' }}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Ações</TableCell>
+                <TableCell>Nome completo</TableCell>
+                <TableCell>Email</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map(row => (
+                <TableRow key={row.id}>
+                  <TableCell>Ações</TableCell>
+                  <TableCell>{row.nomeCompleto}</TableCell>
+                  <TableCell>{row.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
 
       </LayoutBaseDePagina>
     </div>
--- src/shared/services/api/pessoasService/PessoasService.ts
+++ src/shared/services/api/pessoasService/PessoasService.ts
@@ -2,26 +2,26 @@
 import { Api } from '../axios-config/index';
 import { Environment } from '../../../environments/index';
 
-interface IListagemPessoa {
+export interface IListagemPessoa {
   id: number;
   email: string;
   cidadeId: number;
   nomeCompleto: string;
 };
 
-interface IDetalhePessoa {
+export interface IDetalhePessoa {
   id: number;
   email: string;
   cidadeId: number;
   nomeCompleto: string;
 };
 
-type IPessoasComTotalCount = {
+type TPessoasComTotalCount = {
   data: IListagemPessoa[];
   totalCount: number;
 };
 
-const getAll = async (page = 1, filter = ''): Promise<IPessoasComTotalCount | Error> => {
+const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Error> => {
   try {
     const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
 
