d10b3b04c86b122adbd1011c41d4174ce23cc99f
Railson Carvalho <railson031@hotmail.com>
2023-01-31 17:12:18 -0300

    feat: Salvando dados do formulário no bacend

--- src/pages/pessoas/DetalheDePessoas.tsx
+++ src/pages/pessoas/DetalheDePessoas.tsx
@@ -1,16 +1,23 @@
-import { LinearProgress, TextField } from '@mui/material';
-
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { PessoasService } from '../../shared/services';
 import { LayoutBaseDePagina } from '../../shared/layouts';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FerramentasDeDetalhes } from '../../shared/components';
 import { Form } from '@unform/web';
 import { VTextField } from '../../shared/forms';
+import { FormHandles } from '@unform/core';
+
+interface IFormData {
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
 
 export const DetalheDePessoas: React.FC = () => {
   const { id = 'nova' } = useParams<'id'>();
   const navigate = useNavigate();
+
+  const formRef = useRef<FormHandles>(null);
 
   const [isLoading, setIsLoading] = useState(false);
   const [nome, setNome] = useState('');
@@ -28,14 +35,38 @@ export const DetalheDePessoas: React.FC = () => {
             navigate('/pessoas');
           } else {
             setNome(result.nomeCompleto);
-            console.log(result);
+            formRef.current?.setData(result);
           }
         });
     }
   }, [id]);
 
-  const handleSave = () => {
-    console.log('Save')
+  const handleSave = (dados: IFormData) => {
+    setIsLoading(true);
+    if (id === 'nova') {
+      PessoasService
+        .create(dados)
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            navigate(`/pessoas/detalhe/${result}`);
+          }
+        });
+    } else {
+      PessoasService
+        .updateById(Number(id), { id: Number(id), ...dados })
+        .then((result) => {
+          setIsLoading(false);
+
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+          }
+        });
+    }
   };
 
   const handleDelete = (id: number) => {
@@ -62,19 +93,18 @@ export const DetalheDePessoas: React.FC = () => {
           mostrarBotaoNovo={id !== 'nova'}
           mostarBotaoApagar={id !== 'nova'}
 
-          aoClicarEmSalvar={handleSave}
-          aoClicarEmSalvarEVoltar={handleSave}
           aoClicarEmVoltar={() => navigate('/pessoas')}
           aoClicarEmApagar={() => handleDelete(Number(id))}
+          aoClicarEmSalvar={() => formRef.current?.submitForm()}
           aoClicarEmNovo={() => navigate('/pessoas/detalhe/nova')}
+          aoClicarEmSalvarEVoltar={() => formRef.current?.submitForm()}
         />
       }
     >
-      <Form onSubmit={(dados) => console.log(dados)}>
-        <VTextField
-          name='nomeCompleto'
-        />
-        <button type='submit'>Submit</button>
+      <Form ref={formRef} onSubmit={handleSave}>
+        <VTextField placeholder='Nome completo' name='nomeCompleto' />
+        <VTextField placeholder='Email' name='email' />
+        <VTextField placeholder='Cidade id' name='cidadeId' />
       </Form>
     </LayoutBaseDePagina>
   );
